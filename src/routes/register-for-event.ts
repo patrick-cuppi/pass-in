6b3a55a5a0ed12b14645f470957eeea62d8daaf0
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-04-03 22:09:56 -0300

    feat: POST method for registration was created

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function registerForEvent(app: FastifyInstance) {
+    app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/events/:eventId/attendees', { 
+        schema: {
+            body: z.object({
+                name: z.string().min(4),
+                email: z.string().email(),
+            }),
+            params: z.object({
+                eventId: z.string().uuid(),
+            }),
+            response: {
+                201: z.object({
+                    attendeeId: z.number(),
+                })
+            }
+        }
+     }, async (request, reply) => {
+        const { eventId } = request.params
+        const { name, email } = request.body
+
+        const attendeeFromEmail = await prisma.attendee.findUnique({
+            where: {
+                eventId_email: {
+                    email,
+                    eventId
+                }
+            }
+        })
+
+        if (attendeeFromEmail !== null) {
+            throw new Error('This e-mail is already registered for this event.')
+        }
+
+        const attendee = await prisma.attendee.create({
+            data: {
+                name,
+                email,
+                eventId,
+            }
+        })
+
+        return reply.status(201).send({ attendeeId: attendee.id })
+
+    })
+}
